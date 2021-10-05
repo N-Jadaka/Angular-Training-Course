71f83d472fdc20bc7f72b9a14c03b22b484a9325
nezianyajc <80351494+N-Jadaka@users.noreply.github.com>
2021-10-05 07:04:32 -0500

    Created assignment two.
    Worked with databinding and string interpolation. Saving before the next part on directives.

--- section-1-project-setup/my-first-app/src/app/servers/servers.component.ts
+++ section-1-project-setup/my-first-app/src/app/servers/servers.component.ts
@@ -2,14 +2,30 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-servers',
-  template: '<app-server></app-server>',
+  templateUrl: 'servers.component.html',
   styleUrls: ['./servers.component.css']
 })
 export class ServersComponent implements OnInit {
 
-  constructor() { }
+  allowNewServer = false;
+  serverCreationStatus = " No server was created! ";
+  serverName = 'Test Server';
 
-  ngOnInit(): void {
+  constructor() {
+    setTimeout(() => {
+      this.allowNewServer = true;
+    }, 2000);
+   }
+
+  ngOnInit() {
+  }
+
+  onCreateServer(){
+    this.serverCreationStatus = "This server was created dude..." + this.serverName;
+  }
+
+  onUpdateServerName(event: any) {
+    this.serverName = (<HTMLInputElement>event.target).value;
   }
 
 }
