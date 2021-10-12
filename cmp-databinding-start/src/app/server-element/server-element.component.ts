1d939ef6c455f254fd27e7d5d53b622e5090a459
nezianyajc <jnezia1@hotmail.com>
2021-10-12 07:16:26 -0500

    Setup custom event binding.
    Working through section 2.

--- cmp-databinding-start/src/app/server-element/server-element.component.ts
+++ cmp-databinding-start/src/app/server-element/server-element.component.ts
@@ -8,6 +8,7 @@ import { Component, Input, OnInit } from '@angular/core';
 })
   export class ServerElementComponent implements OnInit {
   @Input()  element: {type: string, name: string, content: string};
+  //Can change the name of the element object by passing the input decorator a new name
   constructor() { }
 
   ngOnInit(): void {
