e38f1769b42b9a112624be797301ef00d7d7a5a8
nezianyajc <jnezia1@hotmail.com>
2021-10-12 11:06:28 -0500

    Finished the majority of Section 5
    Finished the majority of Section 5. I have to return to exercise 78.

--- cmp-databinding-start/src/app/server-element/server-element.component.ts
+++ cmp-databinding-start/src/app/server-element/server-element.component.ts
@@ -1,17 +1,43 @@
-import { element } from 'protractor';
-import { Component, Input, OnInit } from '@angular/core';
+import { AfterContentInit, Component, DoCheck, Input, OnChanges, OnInit, SimpleChange, SimpleChanges, ViewEncapsulation } from '@angular/core';
 
 @Component({
   selector: 'app-server-element',
   templateUrl: './server-element.component.html',
-  styleUrls: ['./server-element.component.css']
+  styleUrls: ['./server-element.component.css'],
+  encapsulation: ViewEncapsulation.Emulated //None, ShadowDom
 })
-  export class ServerElementComponent implements OnInit {
+  export class ServerElementComponent implements OnInit, OnChanges, DoCheck, AfterContentInit {
   @Input()  element: {type: string, name: string, content: string};
   //Can change the name of the element object by passing the input decorator a new name
-  constructor() { }
+  @Input() name: string;
 
-  ngOnInit(): void {
+  constructor() {
+    console.log('constructor called!');
   }
 
+  ngOnChanges(changes:SimpleChanges ): void {
+    console.log('ngOnChanges called');
+    console.log(changes);
+
+  }
+
+  ngOnInit(): void {
+    console.log('ngOnInit called');
+
+  }
+
+  ngDoCheck(): void {
+    //Called every time that the input properties of a component or a directive are checked. Use it to extend change detection by performing a custom check.
+    //Add 'implements DoCheck' to the class.
+    console.log("ngDoCheck called");
+
+  }
+
+  ngAfterContentInit(){
+    console.log("ngafter content init");
+  }
+
+  ngAfterContentChecked(): void {
+    console.log("ngafter content checked");
+  }
 }
