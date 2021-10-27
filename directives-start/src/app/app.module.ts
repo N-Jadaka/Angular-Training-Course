21d5972cf7c39660c5835427a0c34388b5c0af24
nezianyajc <80351494+N-Jadaka@users.noreply.github.com>
2021-10-27 15:17:07 -0500

    Finished Directive Section
    Created structural directive and a dropdown for the project.

--- directives-start/src/app/app.module.ts
+++ directives-start/src/app/app.module.ts
@@ -1,13 +1,19 @@
+import { BasicHighlightDirective } from './basic-highlight/basic-highlight.directive';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 
 
 import { AppComponent } from './app.component';
+import { BetterHighlightDirective } from './better-highlight/better-highlight.directive';
+import { UnlessDirective } from './unless.directive';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BasicHighlightDirective,
+    BetterHighlightDirective,
+    UnlessDirective
   ],
   imports: [
     BrowserModule,
