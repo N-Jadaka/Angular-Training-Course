0189b904906fc4e6fe474463b065ad01bccdf01f
nezianyajc <80351494+N-Jadaka@users.noreply.github.com>
2021-11-04 11:48:25 -0500

    Worked with error codes & custom async validators.

--- forms-reactive-start/src/app/app.component.ts
+++ forms-reactive-start/src/app/app.component.ts
@@ -1,3 +1,4 @@
+import { Observable } from 'rxjs';
 import { Component, OnInit } from '@angular/core';
 import { FormArray, FormControl, FormGroup, Validators } from '@angular/forms';
 
@@ -16,15 +17,22 @@ export class AppComponent implements OnInit{
       'userData': new FormGroup({
         'username': new FormControl(null, [Validators.required, this.forbiddenNames.bind(this)]),
         'email': new FormControl(null, [Validators.required,
-          Validators.email])
+          Validators.email], this.forbiddenEmails)
       }),
       'gender': new FormControl('male'),
       'hobbies': new FormArray([])
     });
+    // this.signupForm.valueChanges.subscribe(
+    //   (value) => console.log(value)
+    // );
+    this.signupForm.statusChanges.subscribe(
+      (value) => console.log(value)
+    );
   }
 
   onSubmit(){
     console.log(this.signupForm);
+    this.signupForm.reset();
   }
 
   onAddHobby(){
@@ -37,5 +45,18 @@ export class AppComponent implements OnInit{
       return {'nameIsForbidden': true};
     }
     return null;
+  }
+
+  forbiddenEmails(control: FormControl): Promise<any> | Observable<any> {
+    const promise = new Promise<any>((resolve, reject) => {
+      setTimeout(() => {
+        if(control.value === 'test@test.com'){
+          resolve({'emailIsForbidden' : true});
+        } else {
+          resolve(null);
+        }
+      }, 1500)
+    });
+    return promise;
   }
 }
