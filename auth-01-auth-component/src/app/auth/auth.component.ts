109bb27422567071567f70ca8c248b6860d7b024
nezianyajc <80351494+N-Jadaka@users.noreply.github.com>
2021-12-19 19:46:46 -0600

    Created auth for sign up and login

--- auth-01-auth-component/src/app/auth/auth.component.ts
+++ auth-01-auth-component/src/app/auth/auth.component.ts
@@ -1,7 +1,9 @@
+import { Router } from '@angular/router';
 
 import { NgForm } from '@angular/forms';
 import { Component } from "@angular/core";
-import { AuthService } from './auth.service';
+import { AuthResponseData, AuthService } from './auth.service';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-auth',
@@ -10,8 +12,10 @@ import { AuthService } from './auth.service';
 
 export class AuthComponent {
   isLoginMode = true;
+  isLoading = false;
+  error: string = null;
 
-  constructor(private authService: AuthService){}
+  constructor(private authService: AuthService, private router: Router){}
 
   onSwitchMode() {
     this.isLoginMode = !this.isLoginMode;
@@ -23,20 +27,30 @@ export class AuthComponent {
     }
     const email = form.value.email;
     const password = form.value.password;
+    let authObs: Observable<AuthResponseData>
 
+
+    this.isLoading = true;
     if(this.isLoginMode) {
-
+      authObs = this.authService.login(email, password);
     } else {
+      authObs = this.authService.signup(email, password);
 
-    this.authService.signup(email, password).subscribe(
-      resData => {
-      console.log(resData);
-    }, error => {
-      console.log(error);
     }
 
+    authObs.subscribe(
+      resData => {
+        console.log(resData);
+        this.isLoading = false;
+        this.router.navigate(['/recipes']);
+      },
+       errorMessage => {
+         console.log(errorMessage);
+         this.error = errorMessage;
+         this.isLoading = false;
+       }
     );
-  }
+
     form.reset();
   }
 
