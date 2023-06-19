d0dd02d56ce13adf549cf883f31cb96039f9d43c
Josh N <jnezia1@hotmail.com>
2023-06-19 08:08:51 -0500

    Finished a quarter of the routing section

--- routing-start/src/app/app.module.ts
+++ routing-start/src/app/app.module.ts
@@ -15,7 +15,8 @@ import { ServersService } from './servers/servers.service';
 
 const appRoutes: Routes = [
   {path: '', component: HomeComponent},
-  {path: 'users', component: UserComponent},
+  {path: 'user/:id/:name', component: UserComponent},
+  {path: 'users', component: UsersComponent},
   {path: 'servers', component: ServerComponent},
 
 
